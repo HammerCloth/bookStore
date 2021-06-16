3663ca50ee16e3f0281d4763a496c7d6b9664229
syx <424193726@qq.com>
2021-06-16 19:41:51 +0800

    书城项目

---
+++ src/com/syx/bean/user.java
@@ -0,0 +1,55 @@
+package com.syx.bean;
+
+/**
+ * @author:syx
+ * @date:2021/6/15 20:16
+ * @version:v1.0
+ */
+public class user {
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+
+    public user(Integer id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public user() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/com/syx/dao/userDao.java
@@ -0,0 +1,9 @@
+package com.syx.dao;
+
+/**
+ * @author:syx
+ * @date:2021/6/15 20:41
+ * @version:v1.0
+ */
+public interface userDao {
+}
